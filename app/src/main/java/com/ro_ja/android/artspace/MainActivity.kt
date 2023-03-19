8a49d9c86072fb850581c4eddf438cfc974ca967
Roman <boyra75.rb@gmail.com>
2023-03-19 16:46:48 +0900

    add modifier

--- app/src/main/java/com/ro_ja/android/artspace/MainActivity.kt
+++ app/src/main/java/com/ro_ja/android/artspace/MainActivity.kt
@@ -5,16 +5,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ro_ja.android.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,10 +44,15 @@ fun ArtSpaceApp() {
 }
 
 @Composable
-fun PictureAtt(modifier: Modifier = Modifier){
-    Box(modifier = Modifier.fillMaxSize()) {
-        Image(painter = painterResource(id = R.drawable.mark_24_09_15), contentDescription = stringResource(
-                    R.string.boy)
+fun PictureAtt(){
+    Box(modifier = Modifier.fillMaxWidth()
+        .padding(16.dp)) {
+        Image(painter = painterResource(id = R.drawable.mark_24_09_15),
+            contentDescription = stringResource(R.string.boy),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(RoundedCornerShape(16.dp))
                 )
     }
 }
